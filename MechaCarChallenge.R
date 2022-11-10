69e98c8bb746149e31a461208321f0c7674d781d
Will Enny <enny.williamrobert@gmail.com>
2022-11-09 22:31:49 -0500

    all deliverables complete.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,7 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 Suspension_df <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 total_summary <- Suspension_df %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
 lot_summary <- Suspension_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+t.test(Suspension_df$PSI, mu=1500)
+t.test(subset(Suspension_df$PSI, Suspension_df$Manufacturing_Lot=="Lot1"), mu=1500)
+t.test(subset(Suspension_df$PSI, Suspension_df$Manufacturing_Lot=="Lot2"), mu=1500)
+t.test(subset(Suspension_df$PSI, Suspension_df$Manufacturing_Lot=="Lot3"), mu=1500)
